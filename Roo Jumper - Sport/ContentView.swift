93d7dab464d71ea91be4a298f3907e4dc5a4f5cd
Tymur Batulin <batulintimur@gmail.com>
2026-05-21 13:00:25 +0200

    Initial Commit

---
+++ Roo Jumper - Sport/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Roo Jumper - Sport
+//
+//  Created by Tymur Batulin on 21.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
